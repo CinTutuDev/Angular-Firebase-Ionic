1933300bb21dabacadd4d6af10ca951787ae40e7
cinta <cintatafur@gmail.com>
2023-05-30 12:02:09 +0200

    ✨Import(ReactiveFormsModule):creo e importo form en login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    email : ['', [Validators.email, Validators.required]],
+    pass : ['', [Validators.required]],
+  })
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit() {
   }
