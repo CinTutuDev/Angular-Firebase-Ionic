b19743cfab6e22d195d2e62322ea4e22d583a5bc
cinta <cintatafur@gmail.com>
2023-05-30 16:18:52 +0200

    ✅ firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-firebase-ionic',
+    appId: '1:798805885302:web:57d37d18a0e0b34f9afa57',
+    storageBucket: 'angular-firebase-ionic.appspot.com',
+    apiKey: 'AIzaSyBNG5SaI-9vxkZEwrEvcg5ZWKURPeke71c',
+    authDomain: 'angular-firebase-ionic.firebaseapp.com',
+    messagingSenderId: '798805885302',
+  },
   production: true
 };
