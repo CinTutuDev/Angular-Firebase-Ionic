cd199e48a49ab9515aaf9f85795801c9d448372b
cinta <cintatafur@gmail.com>
2023-05-30 12:26:13 +0200

    ✅ Create(logica):submit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,15 +7,23 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
   form = this.fb.group({
-    email : ['', [Validators.email, Validators.required]],
-    pass : ['', [Validators.required]],
-  })
+    email: ['', [Validators.email, Validators.required]],
+    pass: ['', [Validators.required]],
+  });
 
-  constructor( private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+
+  login(){
+    if(this.form.valid){
+      const {email, pass} = this.form.getRawValue();
+      console.log(email, pass);
+    }else{
+      this.form.markAllAsTouched()
+    }
   }
 
 }
